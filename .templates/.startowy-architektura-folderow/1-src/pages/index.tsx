758c11eb7cd1f8803c3554bf5f8f3bf0c656ab9b
Blendear <tobiascibis@gmail.com>
2022-10-10 01:25:12 +0200

    z templatem z nowa architekturaa

---
+++ .templates/.startowy-architektura-folderow/1-src/pages/index.tsx
@@ -0,0 +1,15 @@
+// //
+// //  1.
+// //
+
+// //
+// //  1.  Klepsydra całego dnia
+// //
+// //      1.1.
+
+// import styles from "../styles/sass/styles-all.module.scss";
+
+// const a = () => {
+//   return <h1 className={styles.eksperymentalny}> ...cias sdteczko </h1>;
+// };
+// export default a;
---
+++ .templates/.startowy-configuration-files-DOPISZ-DO-NICH-TO/DO-tsconfig-json.tsx
@@ -0,0 +1,3 @@
+// pod "scripts" wrzuć to (zmień ściezki, jesli zmieniłeś architekturę folderów, of cors\/
+
+// "compile:sass": "sass --watch src/styles/sass/styles-all.module.scss src/styles/css/styles-all.css"
