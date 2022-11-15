fa9b74b97adb1b833371a74be3b9b6c664b34ba7
Blendear <tobiascibis@gmail.com>
2022-11-15 15:28:40 +0100

    moja wiadomość do commita

--- src/components/1-strona-glowna/1-6-godziny-otwarcia.js
+++ src/components/1-strona-glowna/1-6-godziny-otwarcia.js
@@ -24,8 +24,36 @@
 //
 
 import styles from "src/styles/sass/styles-all.module.scss";
+import { useState } from "react";
 
 const GodzinyOtwarcia = () => {
+  //nazwy kolejnych akordeonów. Kratywnie, ik.
+  const [isOpenM, setIsOpenM] = useState(false);
+  const [isOpenK, setIsOpenK] = useState(false);
+  const [isOpenKoedu, setIsOpenKoedu] = useState(false);
+
+  const handleClick = (ktoryAccordion) => {
+    switch (ktoryAccordion) {
+      case "M":
+        console.log("M");
+        setIsOpenM(!isOpenM);
+        console.log(isOpenM);
+
+        break;
+      case "K":
+        console.log("K");
+        setIsOpenK(!isOpenK);
+        console.log(isOpenK);
+
+        break;
+      case "Koedu":
+        console.log("Koedu");
+        setIsOpenKoedu(!isOpenKoedu);
+        console.log(isOpenKoedu);
+        break;
+    }
+  };
+
   return (
     <h1 className={styles["layout-strona-glowna__godziny-otwarcia"]}>
       <div className={styles["layout-strona-glowna__godziny-otwarcia--sauna"]}>
@@ -48,16 +76,39 @@ const GodzinyOtwarcia = () => {
               ]
             }
           >
-            <div> {"[ ]"}</div>
+            {/* //hook1 - rozdzial z dodawaniem ikonek - doslownie coy paste z heroicon "copy jsx" */}
+            {/* <div onClick={setA(tue)}> /> */}
+            {/* hook1 - /\ NIE WYWOLUJESZ w onlcik. tylko podajesz samą nazwę  funkcji. W niej zrobimy wywołanie set statea*/}
+            <div className={styles["accordion__trigger-od-niego"]}>
+              <svg
+                // className={styles["accordion__trigger-od-niego"]}
+                onClick={() => {
+                  handleClick("M");
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                />
+              </svg>
+            </div>
+
             <div> | mezczyzni</div>
           </div>
           <div
             className={
               styles[
-                "layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion"
+                `layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion${
+                  isOpenM === false ? "--closed" : "--open"
+                }`
               ]
-              // ,
-              // styles["closed"]
             }
           >
             <div>a</div>
@@ -77,17 +128,34 @@ const GodzinyOtwarcia = () => {
               ]
             }
           >
-            <div> {"[ ]"}</div>
+            <div className={styles["accordion__trigger-od-niego"]}>
+              <svg
+                onClick={() => {
+                  handleClick("K");
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                />
+              </svg>
+            </div>
             <div> | kobiety</div>
           </div>
           <div
             className={
               styles[
-                "layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion"
+                `layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion${
+                  isOpenK === false ? "--closed" : "--open"
+                }`
               ]
-              // ,
-              // styles["closed"]
-              ////hook1- /\ dlaczego zmienia KURWA ta druga clasa sposob displayu??
             }
           >
             <div>a</div>
@@ -107,17 +175,34 @@ const GodzinyOtwarcia = () => {
               ]
             }
           >
-            <div> {"[ ]"}</div>
+            <div className={styles["accordion__trigger-od-niego"]}>
+              <svg
+                onClick={() => {
+                  handleClick("Koedu");
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                />
+              </svg>
+            </div>
             <div> | koedukacja</div>
           </div>
           <div
             className={
               styles[
-                "layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion"
+                `layout-strona-glowna__godziny-otwarcia--sauna--lista-plci--accordion${
+                  isOpenKoedu === false ? "--closed" : "--open"
+                }`
               ]
-              // ,
-              // styles["closed"]
-              ////hook1- /\ dlaczego zmienia KURWA ta druga clasa sposob displayu??
             }
           >
             <div>a</div>
