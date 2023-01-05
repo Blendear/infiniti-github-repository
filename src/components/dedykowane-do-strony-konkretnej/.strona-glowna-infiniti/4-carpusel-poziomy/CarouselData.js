a50c79f4cd3dbc6360a65055f4e65bb8d6785133
Blendear <tobiascibis@gmail.com>
2023-01-05 21:00:42 +0100

    kadra 40%

--- src/components/dedykowane-do-strony-konkretnej/.strona-glowna-infiniti/4-carpusel-poziomy/CarouselData.js
+++ src/components/dedykowane-do-strony-konkretnej/.strona-glowna-infiniti/4-carpusel-poziomy/CarouselData.js
@@ -11,25 +11,30 @@
 //
 //       A.1. Trap1 - Staticly imported foty muszą być, żeby next.js'owy Image je załapał
 //
-import foto1 from "../../../../../public/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/minus1.png";
-import foto2 from "../../../../../public/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/0.png";
-import foto3 from "../../../../../public/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/1.png";
-import foto4 from "../../../../../public/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/2.png";
+// import foto1 from "../../../../../public/";
+
+// import foto2 from "../../../../../public/";
+// import foto3 from "../../../../../public";
+// import foto4 from "../../../../../public";
 
 //
 //       A.2. Każdy object w arrayu dostaje "staticcly imported image" jako property "image"
 //
 export const carouselData = [
   {
-    image: foto1,
+    image:
+      "/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/minus1.png",
   },
   {
-    image: foto2,
+    image:
+      "/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/0.png",
   },
   {
-    image: foto3,
+    image:
+      "/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/1.png",
   },
   {
-    image: foto4,
+    image:
+      "/images/.dedykowane-do-strony-konkretnej/1-strona-glowna-infiniti/carousel-poziomy/2.png",
   },
 ];
