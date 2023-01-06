26135e1bfaa2771b0432b1f9c5b2b416276e0e17
Blendear <tobiascibis@gmail.com>
2023-01-06 13:24:34 +0100

    kadra 50%

---
+++ src/components/dedykowane-do-strony-konkretnej/fitness-kadra/shape-lisc.jsx
@@ -0,0 +1,26 @@
+const SVGShapeTrenerzy = () => {
+  //
+  // Trap1 - CAMEL CASE nazewnictwo plików musi być, np.
+  // sketch:type    zamień na    sketchType
+  // xmlns:xlink    zamień na    xmlnsXlink
+  // xlink:href     zamień na    xlinkHref
+  //
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      id="eFrvW0oEsHf1"
+      viewBox="0 0 1080 1450"
+      shape-rendering="geometricPrecision"
+      text-rendering="geometricPrecision"
+    >
+      <path
+        d="M156.839279,1489.311172C135.48263,1457.655521,54.489668,1344.999924,48.96516,1164.262928C45.324458,1045.15562,86.220366,810.177626,156.839279,730.931242C202.929814,612.412723,426.045887,385.010871,588.96516,412.825204c-115.112282,18.64095-96.69244,77.304151-57.460987,135.321802C710.77586,689.368414,756.866395,465.500101,1020.24088,761.796397c99.811199,111.095655,137.679792,517.275099-32.921811,750.617284s208.25735,256.158972,0,335.802469c-244.436393-56.942363-612.398061,38.194314-830.47979-358.904978Z"
+        transform="translate(-48.96516-407.897186)"
+        fill="#37d18d"
+        stroke-width="3.84"
+      />
+    </svg>
+  );
+};
+export default SVGShapeTrenerzy;
