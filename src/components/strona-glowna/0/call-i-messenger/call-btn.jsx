30178db12d9ba2271c51111cdcc933d48ac3ce0c
Blendear <tobiascibis@gmail.com>
2023-02-14 02:03:44 +0100

    call i messenger buttons

--- src/components/strona-glowna/0/call-i-messenger/call-btn.jsx
+++ src/components/strona-glowna/0/call-i-messenger/call-btn.jsx
@@ -1,17 +1,29 @@
 import styles from "src/styles/sass/styles-all.module.scss";
-import { BsTelephone } from "react-icons/bs";
-
+import { BsFillTelephoneFill, BsMessenger } from "react-icons/bs";
+import Link from "next/link";
 const CallButton = (props) => {
   return (
     //bez "Link", bo href telefoniczny tam nie dziala
-    <a
-      href="tel:+48884848808"
-      className={styles["layout__strona-glowna__call-btn"]}
-    >
-      <div className={styles["layout__strona-glowna__call-btn--svg"]}>
-        <BsTelephone />
-      </div>
-    </a>
+    <div>
+      <a
+        href="tel:+48884848808"
+        className={styles["layout__strona-glowna__call-btn"]}
+      >
+        <div className={styles["layout__strona-glowna__call-btn--svg"]}>
+          <BsFillTelephoneFill />
+        </div>
+      </a>
+
+      <a
+        // trap1 - links - zewnetrzne sytrony = MUSISZ DAC HTTPS na start, inaczej bedzie probwac odalic URL jako sufix twojej strony , wiec nie zadzial
+        href="https://m.me/182436331865457"
+        className={styles["layout__strona-glowna__messenger-btn"]}
+      >
+        <div className={styles["layout__strona-glowna__messenger-btn--svg"]}>
+          <BsMessenger />
+        </div>
+      </a>
+    </div>
   );
 };
 export default CallButton;
