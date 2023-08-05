5f91f61dd39ade96223d56cf29825ec07e23a6c5
Blendear <tobiascibis@gmail.com>
2023-08-05 23:02:55 +0200

    feature: let users scan a qr code to find exercises for a specific machine

--- src/features/e-trener/components/QRSzukacz.jsx
+++ src/features/e-trener/components/QRSzukacz.jsx
@@ -2,23 +2,51 @@
 // Table of content for this file is written at the bottom
 //
 import styles from "src/styles/sass/styles-all.module.scss";
-import Link from "next/link";
 import TytulBezTla from "./TytulBezTla";
+import { QrReader } from "react-qr-reader";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 
-const QRSzukacz = ({ propA, propB }) => {
+const QRSzukacz = () => {
+  const router = useRouter();
+
+  const handleScan = (data) => {
+    setResult(data);
+
+    if (data !== null) {
+      router.push(`${data.text}`);
+    }
+  };
   return (
     <div className={styles["container__css-class-name"]}>
       {/* //       _._. AA */}
       <TytulBezTla>ZESKANUJ KOD QR MASZYNY</TytulBezTla>
-      <Link
-        href={`/e-trener/lista-cwiczen?nazwa-modalu=${"null"}&id-cwiczenia=${"null"}`}
-      >
-        <a>do tego linku auto-przeprowadzi QR reader</a>
-      </Link>
+
+      <div>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              handleScan(result);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          constraints={{
+            facingMode: "environment",
+          }}
+        />
+      </div>
     </div>
   );
 };
-export default QRSzukacz;
+// trap1 - NECESSARY to load this page dynamically, because the used QR library wont work server-side (SSR)
+export default dynamic(() => Promise.resolve(QRSzukacz), {
+  ssr: false,
+});
+
+// docs - https://github.com/JodusNodus/react-qr-reader
 
 //~~ _. QR Reader
 //
