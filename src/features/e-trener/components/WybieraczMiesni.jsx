5ace294a6837afdd65bf5dea7ba5e5a7058cd02c
Blendear <tobiascibis@gmail.com>
2023-08-05 11:20:31 +0200

    feature: add photo of 3d model with colored muscle groups
    this will help people to better understand what they train

--- src/features/e-trener/components/WybieraczMiesni.jsx
+++ src/features/e-trener/components/WybieraczMiesni.jsx
@@ -5,15 +5,59 @@ import styles from "src/styles/sass/styles-all.module.scss";
 import TytulBezTla from "./TytulBezTla";
 import { useState } from "react";
 import PotwierdzPrzycisk from "./PotwierdzPrzycisk";
+import Image from "next/image";
+import { grupyMiesniowe } from "..";
 
-const WybieraczMiesni = ({ propA, propB }) => {
+const WybieraczMiesni = () => {
   const [wybranyMiesien, setWybranyMiesien] = useState("");
 
   return (
     <div className={styles["container__css-class-name"]}>
       <TytulBezTla>KLIKNIJ W MIĘSIEŃ & ZATWIERDŹ</TytulBezTla>
+      {console.log("grupyMiesniowe: ", grupyMiesniowe)}
+      <div
+        // className={styles["3d-model-CONTAINER"]}   // \/ is a palceholder, before I write code of <
+        style={{ position: "relative", height: "45rem", aspectRatio: "1/1.5" }}
+      >
+        <Image
+          src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/3d-model.png`}
+          alt={`error while loading image`}
+          layout="fill"
+          objectFit="cover"
+          priority
+          loading="eager"
+          // quality={50}
+        />
+        {/* visuale-wybranych-miesni */}
+        {grupyMiesniowe.map((miesien, index) => (
+          <Image
+            key={index}
+            // public//triceps.png
+            src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/${miesien.fota}`}
+            alt={`error while loading image`}
+            layout="fill"
+            objectFit="cover"
+            priority
+            loading="eager"
+            // quality={50}
+          />
+        ))}
+      </div>
 
-      <div className={styles["child__css-class-name"]}>[Fota interaktywna]</div>
+      {/* niewidzialne buttony, odpalaajkce zmiane opacity danego png'ka miesnia */}
+      <div className={styles["grid-do-btnów"]}>
+        {grupyMiesniowe.map((miesien, index) => (
+          <button
+            key={index}
+            className={styles[`btn--${miesien.fota}`]}
+            onClick={() => {
+              setWybranyMiesien(miesien.nazwa);
+            }}
+          >
+            {`btn--${miesien.fota}`}
+          </button>
+        ))}
+      </div>
 
       {wybranyMiesien && (
         <PotwierdzPrzycisk bgColor="green">{wybranyMiesien}</PotwierdzPrzycisk>
