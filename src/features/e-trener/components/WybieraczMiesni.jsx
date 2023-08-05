69ca348856dbb7383db196758e1c1bd26ebb3668
Blendear <tobiascibis@gmail.com>
2023-08-05 17:18:29 +0200

    feature: let users select a muscle by clicking on it
    in the future it will link to exercises linked to that muscle group

--- src/features/e-trener/components/WybieraczMiesni.jsx
+++ src/features/e-trener/components/WybieraczMiesni.jsx
@@ -7,33 +7,23 @@ import { useState } from "react";
 import PotwierdzPrzycisk from "./PotwierdzPrzycisk";
 import Image from "next/image";
 import { grupyMiesniowe } from "..";
+import grupyMiesnioweDublowaneNaModelu3D from "../data/grupyMiesnioweDublowaneNaModelu3D";
 
 const WybieraczMiesni = () => {
   const [wybranyMiesien, setWybranyMiesien] = useState("");
 
   return (
-    <div className={styles["container__css-class-name"]}>
+    <div className={styles["strona-glowna__wybieracz-miesni"]}>
       <TytulBezTla>KLIKNIJ W MIĘSIEŃ & ZATWIERDŹ</TytulBezTla>
       {console.log("grupyMiesniowe: ", grupyMiesniowe)}
-      <div
-        // className={styles["3d-model-CONTAINER"]}   // \/ is a palceholder, before I write code of <
-        style={{ position: "relative", height: "45rem", aspectRatio: "1/1.5" }}
-      >
-        <Image
-          src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/3d-model.png`}
-          alt={`error while loading image`}
-          layout="fill"
-          objectFit="cover"
-          priority
-          loading="eager"
-          // quality={50}
-        />
-        {/* visuale-wybranych-miesni */}
-        {grupyMiesniowe.map((miesien, index) => (
+      <div>
+        <div
+          className={
+            styles["strona-glowna__wybieracz-miesni__3d-model-z-miesniami"]
+          }
+        >
           <Image
-            key={index}
-            // public//triceps.png
-            src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/${miesien.fota}`}
+            src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/3d-model.png`}
             alt={`error while loading image`}
             layout="fill"
             objectFit="cover"
@@ -41,22 +31,70 @@ const WybieraczMiesni = () => {
             loading="eager"
             // quality={50}
           />
-        ))}
-      </div>
 
-      {/* niewidzialne buttony, odpalaajkce zmiane opacity danego png'ka miesnia */}
-      <div className={styles["grid-do-btnów"]}>
-        {grupyMiesniowe.map((miesien, index) => (
-          <button
-            key={index}
-            className={styles[`btn--${miesien.fota}`]}
-            onClick={() => {
-              setWybranyMiesien(miesien.nazwa);
-            }}
-          >
-            {`btn--${miesien.fota}`}
-          </button>
-        ))}
+          {/* visuale-wybranych-miesni */}
+
+          {grupyMiesniowe.map((miesien, index) => (
+            <div
+              key={index}
+              style={{
+                opacity: `${wybranyMiesien === miesien.nazwa ? 0.5 : 0.1}`,
+                transition: "opacity 0.35s",
+              }}
+            >
+              <Image
+                src={`/images/.dedykowane-do-strony-konkretnej/e-trener/3d-model-grup-miesniowych/${miesien.fota}`}
+                alt={`error while loading image`}
+                layout="fill"
+                objectFit="cover"
+                priority
+                loading="eager"
+                quality={1}
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* niewidzialne buttony, odpalaajkce zmiane opacity danego png'ka miesnia */}
+        <div
+          className={
+            styles["strona-glowna__wybieracz-miesni__siatka-do-przyciskow"]
+          }
+        >
+          {grupyMiesniowe.map((miesien, index) => (
+            <button
+              key={index}
+              className={
+                styles[
+                  `strona-glowna__wybieracz-miesni__siatka-do-przyciskow--${miesien.fota.slice(
+                    0,
+                    -4
+                  )}`
+                ]
+              }
+              onClick={() => {
+                setWybranyMiesien(miesien.nazwa);
+              }}
+            ></button>
+          ))}
+          {/* zdublowanie przyciskow dla niektorych miesni, ponieważ nachodzenie pojedynczych na siebie daloby okropny, nietrafny UX klikajac w nie */}
+          {grupyMiesnioweDublowaneNaModelu3D.map((miesien, index) => (
+            <button
+              key={index}
+              className={
+                styles[
+                  `strona-glowna__wybieracz-miesni__siatka-do-przyciskow--${miesien.fota.slice(
+                    0,
+                    -4
+                  )}`
+                ]
+              }
+              onClick={() => {
+                setWybranyMiesien(miesien.nazwa);
+              }}
+            ></button>
+          ))}
+        </div>
       </div>
 
       {wybranyMiesien && (
---
+++ src/features/e-trener/data/grupyMiesnioweDublowaneNaModelu3D.js
@@ -0,0 +1,19 @@
+const grupyMiesnioweDublowaneNaModelu3D = [
+  { nazwa: "Barki - Tył", fota: "barki-tyl--2.png", indexPrzycisku: 21 },
+  { nazwa: "Barki - Środek", fota: "barki-boki--2.png", indexPrzycisku: 22 },
+  { nazwa: "Barki - Przód", fota: "barki-przod--2.png", indexPrzycisku: 23 },
+  { nazwa: "Biceps", fota: "biceps--2.png", indexPrzycisku: 24 },
+  {
+    nazwa: "Przedramie - Góra",
+    fota: "przedramie-gora--2.png",
+    indexPrzycisku: 25,
+  },
+  {
+    nazwa: "Przedramie - Dół",
+    fota: "przedramie-dol--2.png",
+    indexPrzycisku: 26,
+  },
+  { nazwa: "Triceps", fota: "triceps--2.png", indexPrzycisku: 27 },
+];
+
+export default grupyMiesnioweDublowaneNaModelu3D;
