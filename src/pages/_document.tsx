dffd58ac268d48dce2e7662f1fe3b581961b7911
Blendear <tobiascibis@gmail.com>
2023-08-30 14:14:51 +0200

    chore: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,27 @@ export default function Document() {
       <Head>
         <script async src="https://js.stripe.com/v3/buy-button.js"></script>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         {/*//       _._. "On top of pages content" code, like Modals, Backdrops, Tooltips etc. - the id will be references by React's Portals*/}
