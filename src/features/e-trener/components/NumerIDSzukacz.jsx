7dd838dcd66309b7249ea0f3d3b11f40b99ff0a0
Blendear <tobiascibis@gmail.com>
2024-03-06 15:40:35 +0100

    style(infiniti-app): Hide previous machine image while new image is loading

--- src/features/e-trener/components/NumerIDSzukacz.jsx
+++ src/features/e-trener/components/NumerIDSzukacz.jsx
@@ -1,6 +1,3 @@
-//
-// Table of content for this file is written at the bottom
-//
 import { useState } from "react";
 import styles from "src/styles/sass/styles-all.module.scss";
 import { maszyny } from "..";
@@ -11,10 +8,19 @@ import Image from "next/image";
 const NumerIDSzukacz = () => {
   const [fotaMaszyny, setFotaMaszyny] = useState("");
   const [numerMaszyny, setNumerMaszyny] = useState("");
+  const [
+    najnowszaMaszynaJestJuZZaladowana,
+    setNajnowszaMaszynaJestJuZZaladowana,
+  ] = useState(false);
 
   const handleZmianaNumeru = (e) => {
     let nowaMaszyna;
-    setFotaMaszyny("masz-loader.jpg");
+    setFotaMaszyny("");
+
+    // The image opacity will be "0" until this becomes true,so that
+    // writing "21" after "2" wont show the machine with id "2" before
+    // the machine "21" loads
+    setNajnowszaMaszynaJestJuZZaladowana(false);
 
     e.target.value.length > 0
       ? maszyny.find((maszyna) => {
@@ -33,7 +39,6 @@ const NumerIDSzukacz = () => {
       <TytulBezTla htmlElementType="label">
         WPISZ RĘCZNIE NR MASZYNY
       </TytulBezTla>
-      {/* //       _._. Jeśli użytkownik wpisał 9-cyfrową liczbę, która jest numerem ID maszyny, to wyświetl zdjęcie maszyny. Inaczej - wyświetl zdjęcie z napisem "zły numer"       */}
       <input
         type="number"
         className={styles["strona-glowna__nr-id-szukacz__input-field"]}
@@ -48,9 +53,15 @@ const NumerIDSzukacz = () => {
               className={styles["strona-glowna__nr-id-szukacz__fota-maszyny"]} // \/ is a palceholder, before I write code of <
             >
               <Image
+                style={najnowszaMaszynaJestJuZZaladowana ? {} : { opacity: 0 }}
+                onLoadingComplete={() => {
+                  setNajnowszaMaszynaJestJuZZaladowana(true);
+                  console.log("zaladowany");
+                }}
                 src={`/images/.dedykowane-do-strony-konkretnej/e-trener/maszyny/${
                   fotaMaszyny !== "masz0" ? fotaMaszyny : "masz0.jpg"
                 }`}
+                placeholder="/images/.dedykowane-do-strony-konkretnej/e-trener/maszyny/masz-loader.jpg"
                 alt={`error while loading image`}
                 layout="fill"
                 objectFit="cover"
@@ -73,8 +84,3 @@ const NumerIDSzukacz = () => {
   );
 };
 export default NumerIDSzukacz;
-
-//~~ _. NumerIDSzukacz
-//
-//       _._. Jeśli użytkownik wpisał 9-cyfrową liczbę, która jest numerem ID maszyny, to wyświetl zdjęcie maszyny. Inaczej - wyświetl zdjęcie z napisem "zły numer"
-//
