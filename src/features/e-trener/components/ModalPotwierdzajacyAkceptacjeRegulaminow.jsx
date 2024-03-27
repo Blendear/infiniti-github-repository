f159eccc89e4776a56891f605d00fc9fb046aa14
Blendear <tobiascibis@gmail.com>
2024-03-27 09:22:50 +0100

    feat: Add localStorage functionality to 'terms and conditions & privacy policy' confirmation modal

--- src/features/e-trener/components/ModalPotwierdzajacyAkceptacjeRegulaminow.jsx
+++ src/features/e-trener/components/ModalPotwierdzajacyAkceptacjeRegulaminow.jsx
@@ -1,12 +1,20 @@
-import { useEffect, useState } from "react";
-import { Modal, Backdrop } from "../../../components/e-trener";
+import { useState } from "react";
+import { Modal } from "../../../components/e-trener";
 import Link from "next/link";
 
 const ModalPotwierdzajacyAkceptacjeRegulaminow = ({}) => {
   const [isTermsAndConditionsAccepted, setIsTermsAndConditionsAccepted] =
     useState(false);
+
   const [isPrivacyPolicyAccepted, setIsPrivacyPolicyAccepted] = useState(false);
-  const [isEverythingAccepted, setIsEverythingAccepted] = useState(false);
+
+  const [isEverythingAccepted, setIsEverythingAccepted] = useState(() => {
+    return localStorage.getItem("isEverythingAccepted") || false;
+  });
+
+  const saveAcceptance = () => {
+    localStorage.setItem("isEverythingAccepted", true);
+  };
 
   return (
     <>
@@ -18,37 +26,40 @@ const ModalPotwierdzajacyAkceptacjeRegulaminow = ({}) => {
               <input
                 type="checkbox"
                 name="regulamin"
-                onChange={() => {
-                  setIsTermsAndConditionsAccepted(
-                    !isTermsAndConditionsAccepted
-                  );
+                onChange={(e) => {
+                  setIsTermsAndConditionsAccepted(e.target.checked);
                 }}
               />
+              <p>Regulamin -</p>
               <Link
                 download
                 href="/images/.dedykowane-do-strony-konkretnej/infiniti/do-pobrania/dokumenty/Regulamin_Infiniti_Fitness_Club_2023.pdf"
               >
-                <a target="_blank">Regulamin INFINITI</a>
+                <a target="_blank">Link</a>
               </Link>
             </div>
             <div>
               <input
                 type="checkbox"
                 name="polityka-prywatnosci"
-                onChange={() => {
-                  setIsPrivacyPolicyAccepted(!isPrivacyPolicyAccepted);
+                onChange={(e) => {
+                  setIsPrivacyPolicyAccepted(e.target.checked);
                 }}
               />
+              <p>Polityka Prywatności -</p>
               <Link
                 download
                 href="/images/.dedykowane-do-strony-konkretnej/infiniti/do-pobrania/dokumenty/Polityka_Prywatności_Fit-Food-Group_01.2023.pdf"
               >
-                <a target="_blank">Polityka prywatności</a>
+                <a target="_blank">Link</a>
               </Link>
             </div>
             <button
               onClick={() => {
-                setIsEverythingAccepted(true);
+                saveAcceptance();
+                isTermsAndConditionsAccepted &&
+                  isPrivacyPolicyAccepted &&
+                  setIsEverythingAccepted(true);
               }}
             >
               {isTermsAndConditionsAccepted && isPrivacyPolicyAccepted
