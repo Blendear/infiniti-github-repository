2b3395ec6b165ab7c32654450518cda488b68335
Blendear <tobiascibis@gmail.com>
2025-03-25 09:51:49 +0100

    style(qr-scanner): Update styling

--- src/features/e-trener/components/QRSzukacz.jsx
+++ src/features/e-trener/components/QRSzukacz.jsx
@@ -1,15 +1,8 @@
-//
-// Table of content for this file is written at the bottom
-//
 import styles from "src/styles/sass/styles-all.module.scss";
-import Link from "next/link";
 import TytulBezTla from "./TytulBezTla";
-import { useState } from "react";
-
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { LoaderIcon } from "../../../components/e-trener/LoaderIcon";
-
 import { Scanner } from "@yudiel/react-qr-scanner";
 
 const QRSzukacz = () => {
@@ -17,7 +10,6 @@ const QRSzukacz = () => {
 
   const handleScan = (data) => {
     if (data && data[0] && data[0].rawValue) {
-      // Wyciągamy rawValue z pierwszego elementu w tablicy
       const qrCodeValue = data[0].rawValue;
       router.push(
         `/infiniti-app/lista-cwiczen?filtr=qr&wartosc=${qrCodeValue}&nazwa-modalu=null&id-cwiczenia=null`
@@ -27,24 +19,8 @@ const QRSzukacz = () => {
 
   return (
     <section className={styles["strona-glowna__qr-szukacz__container"]}>
-      {/* //       _._. AA */}
       <TytulBezTla htmlElementType="label">ZESKANUJ KOD QR MASZYNY</TytulBezTla>
       {(
-        // <QrReader
-        //   className={styles["strona-glowna__qr-szukacz__kamera"]}
-        //   onResult={(result, error) => {
-        //     if (!!result) {
-        //       handleScan(result);
-        //     }
-
-        //     if (!!error) {
-        //       // console.info(error);
-        //     }
-        //   }}
-        //   constraints={{
-        //     facingMode: "environment",
-        //   }}
-        // />
         <Scanner
           onScan={(result) => {
             console.log(result), handleScan(result);
@@ -73,14 +49,3 @@ const QRSzukacz = () => {
 export default dynamic(() => Promise.resolve(QRSzukacz), {
   ssr: false,
 });
-
-// docs - https://github.com/JodusNodus/react-qr-reader
-
-//~~ _. QR Reader
-//
-//       _._. AA
-//
-//           _._._. AAA
-//
-//                  _._._._. AAAA
-//
